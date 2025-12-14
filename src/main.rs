e1e4a4d15e20540bfdbb08b250364aa40002004e
Aleksei Batukhtin <full.naked.eye@gmail.com>
2025-12-14 15:34:40 +0500

    fix: diff eq state, collision bodies check

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,49 @@
 mod body;
 mod trajectory;
 use crate::body::body::Body;
-use crate::trajectory::trajectory::calc_gravity_acceleration;
-use nalgebra::{Matrix3, RowVector3, SVector};
+use crate::trajectory::trajectory::{calc_gravity_acceleration, has_collision};
+use nalgebra::{Matrix3, RowVector3, SVector, Vector3};
 use ode_solvers::{Dopri5, System};
 
 /*
-x1 y1 z1 vx1 vy1 vz1
-x2 y2 z2 vx2 vy2 vz2
-x3 y3 z3 vx3 vy3 vz3
+x1  y1  z1  x2  y2  z2  x3  y3  z3
+vx1 vy1 vz1 vx2 vy2 vz2 vx3 vy3 vz3
 --------------------
 total: 18
-(x, y, z) - position
-(x, y, z) - velocity
+(x,  y,  z ) - position
+(vx, vy, vz) - velocity
 */
 
 type State = SVector<f64, 18>;
+
+fn calc_skipper_pos(i: usize) -> usize {
+    i * 3
+}
+
+fn calc_skipper_vel(i: usize) -> usize {
+    (i * 3) + 9
+}
 
 pub fn init_state(bodies: &[Body; 3]) -> State {
     let mut state = State::zeros();
 
     for (i, b) in bodies.iter().enumerate() {
-        let skipper = 6 * i;
-        state[skipper + 0] = b.position.x;
-        state[skipper + 1] = b.position.y;
-        state[skipper + 2] = b.position.z;
-        state[skipper + 3] = b.velocity.x;
-        state[skipper + 4] = b.velocity.y;
-        state[skipper + 5] = b.velocity.z;
-    }
+        let skipper_pos = calc_skipper_pos(i);
+        let skipper_vel = calc_skipper_vel(i);
 
+        state[skipper_pos] = b.position.x;
+        state[skipper_pos + 1] = b.position.y;
+        state[skipper_pos + 2] = b.position.z;
+        state[skipper_vel] = b.velocity.x;
+        state[skipper_vel + 1] = b.velocity.y;
+        state[skipper_vel + 2] = b.velocity.z;
+    }
     state
 }
 
 struct ThreeBodySystem {
     pub mass_mat: Matrix3<f64>,
+    pub bodies: [Body; 3],
 }
 
 impl ThreeBodySystem {
@@ -46,7 +55,10 @@ impl ThreeBodySystem {
 
         let mass_mat = mass_vec.transpose() * RowVector3::<f64>::from_element(1.0);
 
-        Self { mass_mat }
+        Self {
+            mass_mat,
+            bodies: bodies.clone(),
+        }
     }
 }
 
@@ -56,32 +68,42 @@ impl System<f64, State> for ThreeBodySystem {
         let mut pos = Matrix3::<f64>::zeros();
 
         for i in 0..n {
-            pos[(i, i + 0)] = y[i + 0];
-            pos[(i, i + 1)] = y[i + 1];
-            pos[(i, i + 2)] = y[i + 2];
+            let body_pos = RowVector3::<f64>::from_row_slice(&y.as_slice()[(i * 3)..(i * 3 + 3)]);
+            pos.set_column(i, &body_pos.transpose());
         }
 
         let acc = calc_gravity_acceleration(&pos, &self.mass_mat);
 
         for i in 0..n {
-            let base = 6 * i;
+            let skipper_pos = calc_skipper_pos(i);
+            let skipper_vel = calc_skipper_vel(i);
             // y velocity -> dy position
-            dy[base + 0] = y[base + 3];
-            dy[base + 1] = y[base + 4];
-            dy[base + 2] = y[base + 5];
+            dy[skipper_pos] = y[skipper_vel]; // x
+            dy[skipper_pos + 1] = y[skipper_vel + 1]; // y
+            dy[skipper_pos + 2] = y[skipper_vel + 2]; // z
+
             // acc -> dy velocity
-            dy[base + 3] = acc[(i, 0)];
-            dy[base + 4] = acc[(i, 1)];
-            dy[base + 5] = acc[(i, 2)];
+            dy[skipper_vel] = acc[(i, 0)]; // vx
+            dy[skipper_vel + 1] = acc[(i, 1)]; // vy
+            dy[skipper_vel + 2] = acc[(i, 2)]; // vz
         }
+    }
+
+    fn solout(&mut self, _t: f64, y: &State, _dy: &State) -> bool {
+        for i in 0..3 {
+            let body_pos = Vector3::<f64>::from_row_slice(&y.as_slice()[(i * 3)..(i * 3 + 3)]);
+            // update body position;
+            self.bodies[i].position = body_pos;
+        }
+        has_collision(&self.bodies)
     }
 }
 
 fn main() {
     let bodies = [
-        Body::new(1.0, 0.0, 0.0, 1.0, 1.0, 1.0, 1.0, 1.0),
-        Body::new(-1.0, 0.0, 0.0, 1.0, 1.0, 1.0, 1.0, 1.0),
-        Body::new(0.0, 1.0, 0.0, 1.0, 1.0, 1.0, 2.0, 1.0),
+        Body::new(20.0, 0.0, 0.0, 1.0, 0.0, 0.0, 1.0, 1.0),
+        Body::new(0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 1.0),
+        Body::new(-20.0, 0.0, 0.0, 1.0, 0.0, 0.0, 1.0, 1.0),
     ];
 
     let system = ThreeBodySystem::init(&bodies);
@@ -89,4 +111,13 @@ fn main() {
     let mut stepper: Dopri5<f64, State, ThreeBodySystem> =
         Dopri5::new(system, 0.0, 150.0, 0.002, state, 1.0e-14, 1.0e-14);
     let res = stepper.integrate();
+
+    match res {
+        Ok(stats) => {
+            println!("stats: {}", stats);
+        }
+        Err(e) => {
+            println!("e: {}", e);
+        }
+    }
 }
